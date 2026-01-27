8ed50c344d248ad4748c32cdbd66ed1a44ee04f0
Presauced <pre@macbook.local>
2026-01-27 09:28:53 +0800

    update later

--- index.php
+++ index.php
@@ -29,7 +29,27 @@ $issues =
 
 <div class="container">
   <div class="card">
-      about page goes here + list of professors
+    <h1><b>About</b></h1><p>
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. 
+    This is a site. We live in a world where the internet is filled with sites. </p>
+  </div>
+  <div class="card">
+    <h1><b> List of professors </b> </h1>
+    <p> Afterwards, you're supposed to include in a professor's name and make it a hyperlink to their 
+    dedicated subpage where you can then browse their published articles. For e.g.,: </p>
+
+    <p>
+    <a href="">Dr. Jumadi M. Parenreng, spdospdsd</a> | insert a picture of him here by the side </p>
+    <p><a href="">Dr. ABCD, ABCD </a> | insert a picture of him here by the side </p>
+    
   </div>
 </div>
 
